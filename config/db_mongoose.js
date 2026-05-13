bc5a504b387b4ac910cc4c6cb8a747318a709d02
Vinicius Antonio de Oliveira <vinicius.ddev@gmail.com>
2026-05-13 17:21:03 -0300

    feat: mongoose

--- config/db_mongoose.js
+++ config/db_mongoose.js
@@ -5,7 +5,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
-  .then(() => console.log("✅ MongoDB Atlas conectado com sucesso."))
-  .catch((err) => console.error("❌ Erro ao conectar ao MongoDB:", err));
+  .then(() => console.log("MongoDB Atlas conectado com sucesso."))
+  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
 module.exports = mongoose;
